c4959d7974a9c45ea19612f63548f1d2eb142342
Ilya Tsuryev <i.tsuryev@gmail.com>
2021-02-05 21:40:22 +0300

    SFC: CounterButton.vue

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,27 @@
-<template><div></div></template>
+<template>
+  <button @click="increment" type="button">{{ count }}</button>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+
+  props: {
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+
+  methods: {
+    increment: function () {
+      this.$emit('increment', this.count + 1);
+    },
+  },
+};
 </script>
 
 <style></style>
