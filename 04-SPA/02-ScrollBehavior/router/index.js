095ede147487446917aa3b4028ba8fdcec78fc2b
Ilya Tsuryev <i.tsuryev@gmail.com>
2021-02-13 19:58:23 +0300

    02-ScrollBehavior solution

--- 04-SPA/02-ScrollBehavior/router/index.js
+++ 04-SPA/02-ScrollBehavior/router/index.js
@@ -3,8 +3,23 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export function scrollBehavior() {
-  // Место для решения
+export function scrollBehavior(to, from, savedPosition) {
+  if (savedPosition) {
+    return savedPosition;
+  }
+
+  if (to.hash) {
+    return { selector: to.hash };
+  }
+
+  if (
+    to.matched.some((x) => x.meta.saveScrollPosition) &&
+    from.matched.some((x) => x.meta.saveScrollPosition)
+  ) {
+    return false;
+  }
+
+  return { x: 0, y: 0 };
 }
 
 export const router = new VueRouter({
