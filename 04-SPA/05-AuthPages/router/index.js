79af9c0133c8b727d9fde10e6b8bea4057b17a0a
Ilya Tsuryev <i.tsuryev@gmail.com>
2021-02-22 22:45:44 +0300

    05-AuthPages solution

--- 04-SPA/05-AuthPages/router/index.js
+++ 04-SPA/05-AuthPages/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import IndexPage from '../views/IndexPage';
+import LoginPage from '../views/LoginPage';
+import RegisterPage from '../views/RegisterPage';
 
 Vue.use(VueRouter);
 
@@ -9,8 +11,19 @@ export const router = new VueRouter({
   base: '/04-SPA/05-AuthPages',
   routes: [
     {
+      name: 'index',
       path: '/',
       component: IndexPage,
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: LoginPage,
+    },
+    {
+      name: 'register',
+      path: '/register',
+      component: RegisterPage,
     },
   ],
 });
--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,94 @@
 <template>
-  <form class="form"></form>
+  <form class="form" @submit.prevent="onSubmit">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input name="email" type="email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input name="fullname" type="text" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input name="password" type="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input name="passwordRepeated" type="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox"
+        ><input name="agreedWithConditions" type="checkbox" /> Я согласен с
+        условиями <span></span
+      ></label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">
+        Зарегистрироваться
+      </button>
+    </div>
+    <div class="form__append">
+      Уже есть аккаунт?
+      <router-link :to="{ name: 'login' }">Войдите</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+import { register } from '../data';
 
 export default {
   name: 'RegisterPage',
+  methods: {
+    async onSubmit(e) {
+      const formData = new FormData(e.target);
+      const email = formData.get('email');
+      const fullname = formData.get('fullname');
+      const password = formData.get('password');
+      const passwordRepeated = formData.get('passwordRepeated');
+      const agreedWithConditions = formData.get('agreedWithConditions');
+      if (!email) {
+        alert('Требуется ввести Email');
+        return;
+      }
+      if (!fullname) {
+        alert('Требуется ввести полное имя');
+        return;
+      }
+      if (!password) {
+        alert('Требуется ввести пароль');
+        return;
+      }
+      if (password !== passwordRepeated) {
+        alert('Пароли не совпадают');
+        return;
+      }
+      if (!agreedWithConditions) {
+        alert('Требуется согласиться с условиями');
+        return;
+      }
+
+      try {
+        const result = await register(email, fullname, password);
+        // status code is present only in case of errors
+        if (result.statusCode) {
+          alert(result.message);
+        } else {
+          alert(result.id);
+        }
+      } catch (e) {
+        alert('Network error');
+      }
+    },
+  },
 };
 </script>
 
